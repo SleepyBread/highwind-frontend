da49c68587ee55ddc177ab6f09d47a5a1d1726c0
louis-Philippe <louis_Philippe@outlook.com>
2025-02-08 21:54:19 -0500

    I hate angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,12 @@ import { PlanetComponent } from './components/planet/planet.component';
 import { SpaceComponent } from './components/space/space.component';
 import { circularAnimation } from './core/animations/circular.animation';
 import { StarsComponent } from './components/stars/stars.component';
+import {MatGridList, MatGridTile} from '@angular/material/grid-list';
+import {MenuComponent} from './components/Menu/menu/menu.component';
 
 @Component({
     selector: 'app-root',
-    imports: [SpaceComponent, PlanetComponent, StarsComponent],
+    imports: [SpaceComponent, PlanetComponent, StarsComponent, MatGridList, MatGridTile, MenuComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css'
 })
@@ -23,4 +25,6 @@ export class AppComponent {
     public textureSaturn = "../../assets/textures/saturn.jpg";
     public textureUranus = "../../assets/textures/uranus.jpg";
     public textureNeptune = "../../assets/textures/neptune.jpg";
+
+
 }
